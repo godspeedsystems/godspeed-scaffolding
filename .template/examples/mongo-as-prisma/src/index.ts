8599975419000aab9da03156b759dccacdacfb06
gurjotkaur20 <gurjotkaur20@gmail.com>
2024-03-03 12:18:17 +0530

    fix: add otel package initialization

--- .template/examples/mongo-as-prisma/src/index.ts
+++ .template/examples/mongo-as-prisma/src/index.ts
@@ -1,3 +1,13 @@
+try {
+    if (process.env.OTEL_ENABLED == 'true') {
+        require('@godspeedsystems/tracing').initialize();
+    }
+} catch (error) {
+    console.error("OTEL_ENABLED is set, unable to initialize opentelemetry tracing.");
+    console.error(error);
+    process.exit(1);
+}
+
 import Godspeed from "@godspeedsystems/core";
 
 // create a godspeed
